4e16fff21a2b9b2b1f2cc4a23faecac1a58361f7
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-08-10 15:04:28 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [UserController::class, 'store']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::post('/login', [UserController::class, 'authenticate']);
+
+// Route::post('/register', function (Request $request) {
+//     return "halo";
+// });
