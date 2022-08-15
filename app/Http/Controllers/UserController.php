cb73f06951f6620a72423f2a166c2dbdcb9e064d
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-08-15 12:49:49 +0700

    update controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
         return User::all();
     }
