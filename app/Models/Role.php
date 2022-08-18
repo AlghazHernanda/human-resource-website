b190aa5a0c8570e760077d714c123b439905b735
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-08-18 15:00:44 +0700

    protect id

---
+++ app/Models/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    // biar gapake fillable, ini artinya cuma id doang yg gaboleh di masukin mass assigment
+    protected $guarded = ['id'];
+
+    use HasFactory;
+}
