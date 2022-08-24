29a4b28140ecd0b192985eca851efcd9392d1c01
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-08-24 20:51:05 +0700

    update api

--- server/routes/api.php
+++ server/routes/api.php
@@ -38,6 +38,6 @@ Route::post('/storeEmployee', [HRController::class, 'storeEmployee']);
 Route::get('/editEmployee/{employee:id}', [EmployeeController::class, 'edit']);
 Route::put('/editEmployee/{employee:id}', [EmployeeController::class, 'update']);
 
-// Route::post('/register', function (Request $request) {
-//     return "halo";
-// });
+Route::post('/register', function (Request $request) {
+    return "halo";
+});
