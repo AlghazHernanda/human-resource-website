12419461cf4f8e91a2834668b2d947be9b88d701
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-09-10 17:03:33 +0700

    update relation

--- server/app/Models/Program.php
+++ server/app/Models/Program.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Model
 {
     use HasFactory;
+
+    // biar gapake fillable, ini artinya cuma id doang yg gaboleh di masukin mass assigment
+    protected $guarded = ['id'];
+
+    public function division_employee_program()
+    {
+        //relasi one to one
+        //namanya jadi division_employee, dan mengambil user id
+        return $this->belongsTo(Division::class, 'division_id');
+    }
+    public function role_employee_program()
+    {
+        //relasi one to one
+        //namanya jadi role_employee, dan mengambil user id
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function hr_picprogram()
+    {
+        //relasi one to one
+        //namanya jadi role_employee, dan mengambil user id
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
