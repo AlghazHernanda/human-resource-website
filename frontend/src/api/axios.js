e2d5bb2a38e8ed7fcc3b18117bbc6c41288cc645
Riyadh <riyadh11@live.com>
2022-10-06 13:16:26 +0700

    fix the mess

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://127.0.0.1:8000/api/login';
+const BASE_URL = 'http://localhost:8080/api/login';
 
 export default axios.create({
     baseURL: BASE_URL
